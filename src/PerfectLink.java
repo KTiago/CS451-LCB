1614a73300855f59c97b6ca289e1d04042b01b50
KTiago <tiago.kieliger@gmail.com>
2018-10-16 09:36:46 +0200

    Start of perfect link implementation

--- src/PerfectLink.java
+++ src/PerfectLink.java
@@ -4,32 +4,49 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 
 public class PerfectLink {
 
     private InetAddress sourceIP;
     private int sourcePort;
-    private InetAddress destinationIP;
-    private int destinationPort;
     private DatagramSocket socket;
+    private HashMap<Integer, Link> links;
 
-    public PerfectLink(String sourceIP, int sourcePort, String destinationIP, int destinationPort) throws Exception {
+    public PerfectLink(String sourceIP, int sourcePort) throws Exception {
         this.sourceIP = InetAddress.getByName(sourceIP);
         this.sourcePort = sourcePort;
-        this.destinationIP = InetAddress.getByName(destinationIP);
-        this.destinationPort =  destinationPort;
         this.socket = new DatagramSocket(this.sourcePort, this.sourceIP);
+        this.links = new HashMap<>();
     }
 
+    public void addLink(int destinationID, String destinationIP, int destinationPort)throws Exception{
+        links.put(destinationID, new Link(destinationIP, destinationPort));
+    }
+
+
+    /*
     public void send(String message) throws Exception{
         byte buffer[] = message.getBytes();
         DatagramPacket packet = new DatagramPacket(buffer,message.length(),destinationIP,destinationPort);
         socket.send(packet);
     }
+    */
 
+    /*
     public String receive(int length) throws Exception{
         DatagramPacket packet = new DatagramPacket(new byte[length],length);
         socket.receive(packet);
         return new String(packet.getData());
+    }
+    */
+
+    class Link {
+        private InetAddress destinationIP;
+        private int destinationPort;
+        public Link(String destinationIP, int destinationPort) throws Exception{
+            this.destinationIP = InetAddress.getByName(destinationIP);;
+            this.destinationPort = destinationPort;
+        }
     }
 }
