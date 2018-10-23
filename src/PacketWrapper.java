5930e4fef3e50e01c1a017aafa4f5930d629eb36
root <root@localhost.localdomain>
2018-10-23 11:21:50 +0200

    Adding Packet wrapper

--- src/PacketWrapper.java
+++ src/PacketWrapper.java
@@ -1,14 +1,16 @@
 import java.net.DatagramPacket;
 import java.net.InetAddress;
-import org.apache.lang.ArrayUtils;
+import java.util.Arrays;
 
 public class PacketWrapper {
 
+    private final int SIZEHEADER = 5;
     private Boolean isACK;
     private int sequenceNumber;
     private InetAddress IP;
     private int port;
     private DatagramPacket packet;
+    private String data;
 
     public PacketWrapper(DatagramPacket packet) {
         this.packet = packet;
@@ -36,6 +38,12 @@ public class PacketWrapper {
         return port;
     }
 
+    //Return the content of a packet
+    //It is null if it is an ACK packet
+    public String getData() {
+        return data;
+    }
+
     //Read packet content in order to know if it is an ACK and to get sequence number and content
     private void readPacketContent(){
         byte[] content = packet.getData();
@@ -44,19 +52,21 @@ public class PacketWrapper {
         //If the ACkbyte is 6 then the packet is an ACK packet
         if(ACKbyte == 6){
             this.isACK = true;
+            this.data = null;
         } else {
             this.isACK = false;
+            this.data = toString(content,SIZEHEADER,packet.getLength()-SIZEHEADER);
         }
         this.sequenceNumber = toInt(content,1);
 
     }
 
-    //Return a
-    private String toString(byte[] bytes,int length ,int offset){
+    //Return a String of a slice of an byte of array
+    private String toString(byte[] bytes, int offset, ,int length) {
         //ISO-8859-1 is the default charset encoding
-        return new String(ArrayUtils.subarray(bytes, offset, offset+length), "ISO-8859-1");
+            return new String(Arrays.copyOfRange(bytes, offset, offset + length));
     }
-
+    
     //Return the integer corresponding to 4 bytes in an array at a given offset
     private int toInt(byte[] bytes, int offset) {
         int ret = 0;
