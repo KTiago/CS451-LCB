4f0314597fb71a425a1d5538e12d2b6e5e0e1e02
BennoSchneeberger <benno.schneeberger@epfl.ch>
2018-11-02 11:25:43 +0100

    make file and print logs

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception{
         String membership = "";
         int id_process = 0;
+        int numberMessages = 0;
         //Check the number of arguments
-        int numberArgs = 2;
+        int numberArgs = 3;
         if(args.length != numberArgs){
             System.out.println("Not the right number of arguments");
             System.out.println("--Usage: java Main id_process membership_name");
@@ -13,6 +14,7 @@ public class Main {
         }else{
             id_process = Integer.parseInt(args[0]);
             membership = args[1];
+            numberMessages = Integer.parseInt(args[2]);
         }
         //-- Use ParserMembership class to parse the membership table --
         ParserMembership parser = new ParserMembership(membership);
--- src/da_proc.java
+++ src/da_proc.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.io.FileWriter;
 import java.util.HashMap;
 
 //USERNAME:da-user
@@ -9,6 +11,8 @@ public class da_proc {
     private int id;
     private boolean isWaiting = true;
     private HashMap<Integer, Pair<String,Integer>> membership;
+    private boolean wait_for_start = true;
+    private ArrayList<Pair<Integer,Integer>> logs = new ArrayList<>();
 
     //Constructor of da_proc
     public da_proc(int id,HashMap<Integer, Pair<String,Integer>> membership) {
@@ -28,6 +32,11 @@ public class da_proc {
     //Method invoked when the signal SIGTERM or SIGINT is received
     public void stop(){
         //TODO
+        System.out.println("Immediately stopping network packet processing.\n");
+
+
+        System.out.println("Writing output.");
+        printLogs();
         System.exit(0);
     }
 
@@ -39,5 +48,36 @@ public class da_proc {
         signalhandler.listenTo("TERM");
         signalhandler.listenTo("USR2");
         signalhandler.listenTo("INT");
+    }
+
+    //Callback method for URB
+    private void deliver(int id, int sequenceNumber, String message){
+        //TODO Do something with message?
+        logs.add(Pair.of(id,sequenceNumber));
+    }
+
+    //Print the log file in a output file.
+    private void printLogs() {
+        try {
+            String namefile = "da_proc"+id+".out";
+            int size = logs.size();
+            FileWriter writer = new FileWriter(namefile);
+            for (int i = 0; i < size;i++) {
+                Pair<Character, Integer> l = logs.get(i);
+                if(l.first == id){
+                    writer.write("b " + l.second);
+                }else {
+                    writer.write("d " +l.first+" "+l.second);
+                }
+
+                if(i < size-1){
+                    writer.write("\n");
+                }
+            }
+            writer.close();
+        } catch (Exception e) {
+            System.err.format("Exception occurred trying to write output file for process "+id);
+            e.printStackTrace();
+        }
     }
 }
