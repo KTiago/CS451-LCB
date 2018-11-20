a6eec0d4fb68b5ec341fc4170c2ed3b1a13f605c
KTiago <tiago.kieliger@gmail.com>
2018-11-20 10:47:02 +0100

    Implemented part of the Localized Causal Broadcast class

--- src/Da_proc.java
+++ src/Da_proc.java
@@ -38,24 +38,24 @@ public class Da_proc {
     private volatile boolean isWaiting = true;
     private int id;
     private ArrayList<Pair<Integer,Integer>> logs = new ArrayList<>();
-    private FIFOBroadcast FIFO;
+    private LCBroadcast LCB;
 
     //Constructor of Da_proc
     public Da_proc(int id,HashMap<Integer, Pair<String,Integer>> membership, int numberMessages) throws Exception{
         this.id = id;
         this.numberMessages = numberMessages;
-        FIFO = new FIFOBroadcast(membership,id,this);
+        LCB = new LCBroadcast(membership, null, id,this);
         signalHandling();
     }
 
 
     //Start the process and wait until the USR2 signal is received, when USR2 is received, it will start broadcast
     public void start() throws Exception{
-        FIFO.start();
+        LCB.start();
         //Waiting to get USR2
         wait.await();
         for (int i = 0;i < numberMessages;++i){
-            FIFO.broadcast(" ");
+            LCB.broadcast(" ");
             logs.add(Pair.of(-1,i + 1));
         }
     }
@@ -69,7 +69,7 @@ public class Da_proc {
     //Method invoked when the signal SIGTERM or SIGINT is received
     public void stop(){
         wait.countDown();
-        FIFO.stop();
+        LCB.stop();
         printLogs();
         terminate.countDown();
 
---
+++ src/LCBroadcast.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+public class LCBroadcast {
+
+    private final UniformReliableBroadcast urb;
+    private final Da_proc proc;
+    private HashMap<Pair<Integer, Integer>, Pair<int[], String>> pending;
+    private int[] vectorClock;
+    private List<Integer> dependencies;
+    private int lsn;
+    private int selfId;
+
+    /*
+        FIFO broadcast that uses a Uniform Reliable Broadcast and orders messages to ensure FIFO properties.
+    */
+    public LCBroadcast(HashMap<Integer, Pair<String, Integer>> peers, List<Integer> dependencies, int selfId, Da_proc proc) throws Exception {
+        this.urb = new UniformReliableBroadcast(peers, selfId, this);
+        this.dependencies = dependencies;
+        this.selfId = selfId;
+        this.lsn = 1;
+        this.proc = proc;
+        this.pending = new HashMap<>();
+        this.vectorClock = new int[peers.size() + 1];
+        Arrays.fill(vectorClock, 1);
+    }
+
+    public void start() {
+        urb.start();
+    }
+
+    public void stop() {
+        urb.stop();
+    }
+
+    // broadcast method used by the upper layer to broadcast messages
+    public void broadcast(String message) {
+        synchronized (this) {
+            int[] vectorClockCopy = vectorClock.clone();
+            vectorClockCopy[selfId] = lsn;
+            for (int i : dependencies) {
+                vectorClockCopy[i] = 1;
+            }
+            lsn += 1;
+            urb.broadcast(vcToString(vectorClockCopy) + message);
+        }
+    }
+
+    private int[] getVectorClock(String message) {
+        return null;
+    }
+
+    private String vcToString(int[] vectorClock) {
+        return null;
+    }
+
+    private boolean isSmaller(int[] vc1, int[] vc2){
+        for(int i = 0; i < vc1.length; i++){
+            if(vc1[i] > vc2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // deliver method used by the lower layer (uniform reliable broadcast) to deliver messages
+    // the algorithm store messages that are out of sequence to deliver them when possible.
+    public void deliver(int id, int sequenceNumber, String message) {
+        synchronized (this) {
+            int[] receivedVectorClock = getVectorClock(message);
+            pending.putIfAbsent(Pair.of(id, sequenceNumber), Pair.of(receivedVectorClock, message));
+            Iterator<Pair<Integer, Integer>> iterator = pending.keySet().iterator();
+            while (iterator.hasNext()) {
+                Pair<Integer, Integer> pair = iterator.next();
+                if (isSmaller(pending.get(pair).first, vectorClock))
+                    iterator.remove();
+                    vectorClock[pair.first] += 1;
+                    proc.deliver(pair.first, pair.second, pending.get(pair).second);
+            }
+        }
+    }
+}
